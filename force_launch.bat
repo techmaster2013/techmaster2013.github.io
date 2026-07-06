8119c8c98a1c9b27665ff16c31b1b8684b800f81
h_z13 <zahriyehhamoody@gmail.com>
2026-07-06 12:26:42 -0400

    Delete force_launch.bat

--- force_launch.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cls
-set LOCALAPPDATA=%cd%
-set APPDATA=%cd%
-mkdir "Versions"
-mkdir "Versions\version-2e8286e986f84987"
-copy /y RobloxStudioLauncherBeta.exe "Versions\version-2e8286e986f84987\"
-cd "Versions\version-2e8286e986f84987"
-start RobloxStudioLauncherBeta.exe -ide
